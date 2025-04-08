cdbdca9d1dc1ac2d7ee7588ae3b29e2823d4f960
Simone-Capp <simone.capp@levellingup.gov.uk>
2025-04-08 11:55:02 +0100

    test comparison table queries

--- tests/testthat/test-create_comparison_table.R
+++ tests/testthat/test-create_comparison_table.R
@@ -1,6 +1,7 @@
 test_that("Connection is created", {
   result<- create_comparison_table()
+  print(result)
   expect_false(is.null(sqlhelper::connection_info()))
-  expect_gt(nrow(result), 0) #expect resulting dataframe to have rows greater than 0
-
+  expect_gt(nrow(result$test), 0) #expect resulting dataframe to have rows greater than 0
+  expect_equal(length(result), 3) #expect that result is three components
 })
